408a187040dc600cefe0811bb9ef710ed83a1d20
oscar-LCP <ofcruz@uniboyaca.edu.co>
2025-11-22 16:38:26 -0500

    Se configuró de manera correcta el index.php del microservicio airline

--- back/airline/public/index.php
+++ back/airline/public/index.php
@@ -1,17 +1,44 @@
 <?php
-
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use App\Middlewares\Cors;
 
 require __DIR__ . '/../vendor/autoload.php';
+
 require __DIR__ . '/../app/Config/database.php';
-
-$cors = require __DIR__.'/../app/Middleware/Cors.php';
-$endpoints = require __DIR__ . '/../app/Config/routers.php';
-
 
 $app = AppFactory::create();
 
-$endpoints($app);
-$cors($app);
+$app->add(new Cors());
+
+$app->add(function (Request $request, $handler) {
+    if ($request->getMethod() === 'OPTIONS') {
+        $response = new \Slim\Psr7\Response();
+        return $response->withStatus(200);
+    }
+
+    $headers = $request->getHeader('Authorization');
+    $token = $headers[0] ?? null;
+    if (!$token) {
+        $response = new \Slim\Psr7\Response();
+        $response->getBody()->write(json_encode(['error' => 'Token no provisto']));
+        return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
+    }
+
+    $userRepo = new \App\Repositories\UserRepository();
+    $user = $userRepo->getByToken($token);
+    if (!$user) {
+        $response = new \Slim\Psr7\Response();
+        $response->getBody()->write(json_encode(['error' => 'Token inválido']));
+        return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
+    }
+
+    $request = $request->withAttribute('user', $user);
+
+    return $handler->handle($request);
+});
+
+(require __DIR__ . '/../app/Config/routers.php')($app);
 
 $app->run();
