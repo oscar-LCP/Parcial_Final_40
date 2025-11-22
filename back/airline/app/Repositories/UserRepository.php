b6063aa33cb772729155800c87ad996b736467fb
oscar-LCP <ofcruz@uniboyaca.edu.co>
2025-11-22 16:37:47 -0500

    Se configuró el repositorio de usuario para el microservicio airline

--- back/airline/app/Repositories/UserRepository.php
+++ back/airline/app/Repositories/UserRepository.php
@@ -1,53 +1,10 @@
 <?php
 namespace App\Repositories;
 
-use App\Controllers\UsersController;
-use Exception;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Models\User;
 
-class UserRepository
-{
-
-    private $codesError = [
-        1 => 401,
-        'default' => 400
-    ];
-
-    public function login(Request $request, Response $response)
-    {
-        try {
-            $body = $request->getBody()->getContents();
-            $data = json_decode($body, true);
-            $controller = new UsersController();
-            $user = $controller->login($data['user'], $data['pwd']);
-            $response
-                ->withHeader('Content-Type', 'application/json')
-                ->getBody()
-                ->write($user);
-            return $response;
-        } catch (Exception $ex) {
-            $status =  $this->codesError[$ex->getCode()] ?? $this->codesError['default'];
-            return $response->withStatus($status);
-        }
+class UserRepository {
+    public function getByToken(string $token) {
+        return User::where('token', $token)->first();
     }
-
-    public function queryAllUsers(Request $request, Response $response){
-        try {
-            $controller = new UsersController();
-            $users = $controller->getUsers();
-            if(empty($users)){
-                return $response->withStatus(204);
-            }
-            $response
-                ->withHeader('Content-Type', 'application/json')
-                ->getBody()
-                ->write($users);
-            return $response;
-        } catch (Exception $ex) {
-            $status =  $this->codesError[$ex->getCode()] ?? $this->codesError['default'];
-            return $response->withStatus($status);
-        }
-    }
-
 }
